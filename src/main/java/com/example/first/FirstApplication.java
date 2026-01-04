5c69e4add5a63d5ec3a41c9c9b4fe963f5c20eac
Harshal <harshalgourkar1804@gamil.com>
2026-01-05 00:41:41 +0530

    first commit

---
+++ src/main/java/com/example/first/FirstApplication.java
@@ -0,0 +1,19 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class
+		}
+)
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
